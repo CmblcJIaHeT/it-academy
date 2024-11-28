e1bc24b1d4ca71db313a991eaa784c8853b2ba3c
CmblcJIaHeT <sikvikrwaf@gmail.com>
2024-11-28 19:52:30 +0500

    ...

--- zadachi/25index.js
+++ zadachi/25index.js
@@ -4,18 +4,53 @@ let span2 = document.getElementById("d2")
 let spa1 = document.getElementById("span1")
 let spa3 = document.getElementById("span3")
 let spa2 = document.getElementById("span2")
+let clearBtn = document.getElementById("clear")
+let but1 = document.getElementById("but1")
+let but3 = document.getElementById("but3")
+let but2 = document.getElementById("but2")
 
 function changeParagraphText() {
     spa1.innerText = span1.value
 }
-span1.addEventListener('click', changeParagraphText)
+but1.addEventListener('click', changeParagraphText)
 
 function changeParagraphText1() {
     spa3.innerText = span3.value
 }
-span3.addEventListener('click', changeParagraphText1)
+but3.addEventListener('click', changeParagraphText1)
 
 function changeParagraphText2() {
     spa2.innerText = span2.value
 }
-span2.addEventListener('click', changeParagraphText2)
+but2.addEventListener('click', changeParagraphText2)
+
+function clearAll() {
+    span1.value = ''
+}
+clearBtn.addEventListener('click', clearAll)
+
+function clearAll2() {
+    span2.value = ''
+}
+clearBtn.addEventListener('click', clearAll2)
+
+function clearAll3() {
+    span3.value = ''
+}
+clearBtn.addEventListener('click', clearAll3)
+
+function clearAll45() {
+    spa1.innerText = ''
+}
+clearBtn.addEventListener('click', clearAll45)
+
+function clearAll25() {
+    spa2.innerText = ''
+}
+clearBtn.addEventListener('click', clearAll25)
+
+function clearAll33() {
+    spa3.innerText = ''
+}
+clearBtn.addEventListener('click', clearAll33)
+
---
+++ zadachi/26html.js
@@ -0,0 +1,44 @@
+let span1 = document.getElementById("ud11")
+let span2 = document.getElementById("ud12")
+let span3 = document.getElementById("ud13")
+let spa1 = document.getElementById("d1")
+let spa2= document.getElementById("d2")
+let spa3 = document.getElementById("d3")
+let but23 = document.getElementById("but1")
+let but24 = document.getElementById("but2")
+let but25 = document.getElementById("but3")
+let clearBtn = document.getElementById("clear")
+
+function changeParagraphText1() {
+    span1.innerText = spa1.value
+}
+but23.addEventListener('click', changeParagraphText1)
+
+function changeParagraphText2() {
+    span2.innerText = spa2.value
+}
+but24.addEventListener('click', changeParagraphText2)
+
+function changeParagraphText3() {
+    span3.innerText = spa3.value
+}
+but25.addEventListener('click', changeParagraphText3)
+
+
+function clearAll1() {
+    span1.innerText = ''
+}
+clearBtn.addEventListener('click', clearAll1)
+
+
+function clearAll2() {
+    span2.innerText = ''
+}
+clearBtn.addEventListener('click', clearAll2)
+
+
+function clearAll3() {
+    span3.innerText = ''
+}
+clearBtn.addEventListener('click', clearAll3)
+
