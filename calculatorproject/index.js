46cc8adcad27bd6649fd0b53dde82ee15f933ffa
CmblcJIaHeT <sikvikrwaf@gmail.com>
2024-12-05 19:55:21 +0500

    ...

--- calculatorproject/index.js
+++ calculatorproject/index.js
@@ -1,4 +1,6 @@
-input = documet.getElementById("input")
+let input = document.getElementById("input")
+let clearBtn = document.getElementById("clear")
+let clearBt = document.getElementById("clear1")
 
 //Функция добавления элемента в input
 //value-аргемент функции
@@ -8,6 +10,31 @@ function appendValue(value){
 
 //Функция для рассчета
 function compute(){
-    //eval -  функция  для выполнения JavaScript-кода
-    input.value = eval(input.value);
+    try {
+        //eval -  функция  для выполнения JavaScript-кода
+        input.value = eval(input.value);
+    }
+    catch(e) {
+        input.value = 'Ошибка!'
+    }
 }
+
+function clearAll() {
+    input.value = ''
+}
+clearBtn.addEventListener('click',clearAll )
+
+function backSpace() {
+    input.value = input.value.slice(0, -1)
+}
+
+clearBt.addEventListener('click', backSpace)
+
+
+function checkKey1(event) {
+    let keys = ['1', '2', '3', '4', '5', '6', '7', '8', '9','0'];
+    if (keys.includes(event.key) == false) {
+        event.preventDefault();
+    }
+}
+input.addEventListener('keypress', checkKey1)
