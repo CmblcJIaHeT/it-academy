caea8ce0547dacaf456fe25ba21f441a9bf2a222
CmblcJIaHeT <sikvikrwaf@gmail.com>
2024-12-10 19:55:37 +0500

    ...

---
+++ sdsds/index.js
@@ -0,0 +1,34 @@
+let spa1 = document.getElementById("inp1")
+let spa2 = document.getElementById("inp2")
+let spa3 = document.getElementById("inp3")
+let spa4 = document.getElementById("inp4")
+let spa5 = document.getElementById("inp5")
+let but1 = document.getElementById("but1")
+let but2 = document.getElementById("but2")
+
+function changeParagraphText1(){
+    spa1.innerText =
+}
+but1.addEventListener("click",changeParagraphText1)
+
+function changeParagraphText2(){
+    spa2.innerText =
+}
+but1.addEventListener("click",changeParagraphText2)
+
+function changeParagraphText3(){
+    spa3.innerText =
+}
+but1.addEventListener("click",changeParagraphText3)
+
+function changeParagraphText4(){
+    spa4.innerText =
+}
+but1.addEventListener("click",changeParagraphText4)
+
+function changeParagraphText5(){
+    spa5.innerText =
+}
+but1.addEventListener("click",changeParagraphText5)
+
+
