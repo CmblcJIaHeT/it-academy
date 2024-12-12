1b598380d94fc6c3e6d93b01f8e362f337de503a
CmblcJIaHeT <sikvikrwaf@gmail.com>
2024-12-12 19:56:33 +0500

    ...

--- sdsds/index.js
+++ sdsds/index.js
@@ -6,29 +6,44 @@ let spa5 = document.getElementById("inp5")
 let but1 = document.getElementById("but1")
 let but2 = document.getElementById("but2")
 
-function changeParagraphText1(){
-    spa1.innerText =
-}
-but1.addEventListener("click",changeParagraphText1)
+// function changeParagraphText1(){
+//     spa1.innerText =
+// }
+// but1.addEventListener("click",changeParagraphText1)
+//
+// function changeParagraphText2(){
+//     spa2.innerText =
+// }
+// but1.addEventListener("click",changeParagraphText2)
+//
+// function changeParagraphText3(){
+//     spa3.innerText =
+// }
+// but1.addEventListener("click",changeParagraphText3)
+//
+// function changeParagraphText4(){
+//     spa4.innerText =
+// }
+// but1.addEventListener("click",changeParagraphText4)
+//
+// function changeParagraphText5(){
+//     spa5.innerText =
+//
+// }
+// but1.addEventListener("click",changeParagraphText5)
 
-function changeParagraphText2(){
-    spa2.innerText =
-}
-but1.addEventListener("click",changeParagraphText2)
 
-function changeParagraphText3(){
-    spa3.innerText =
+let button = document.getElementById('but1');
+let elements = document.getElementById('elements');
+let elementNumber = 0;
+function appendElement() {
+    const newDiv = document.createElement('div');
+    newDiv.classList.add('element');
+    elementNumber = elementNumber + 1;
+    newDiv.innerHTML = `<span>Новый элемент №${ elementNumber }</span>`;
+    elements.append(newDiv);
 }
-but1.addEventListener("click",changeParagraphText3)
 
-function changeParagraphText4(){
-    spa4.innerText =
-}
-but1.addEventListener("click",changeParagraphText4)
-
-function changeParagraphText5(){
-    spa5.innerText =
-}
-but1.addEventListener("click",changeParagraphText5)
+button.addEventListener('click', appendElement);
 
 
