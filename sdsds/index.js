d48de7f0e348c507f38d95d044c959dcde0e880f
CmblcJIaHeT <sikvikrwaf@gmail.com>
2025-01-09 19:55:26 +0500

    ...

--- sdsds/index.js
+++ sdsds/index.js
@@ -5,29 +5,34 @@ let spa4 = document.getElementById("inp4")
 let spa5 = document.getElementById("inp5")
 let but1 = document.getElementById("but1")
 let but2 = document.getElementById("but2")
-
+let uvt = document.getElementById('uvt')
 
 let button = document.getElementById('but1');
 let elements = document.getElementById('elements');
-let deleteButton = document.createElement('deleteButton')
 function appendElement() {
     const newDiv = document.createElement('div');
     newDiv.innerHTML += `<div>${spa1.value}</div>`;
-    newDiv.innerHTML += `<div>Цена${spa2.value}</div>`;
-    newDiv.innerHTML += `<div>Количество${spa3.value}</div>`;
-    newDiv.innerHTML += `<div>Артикул${spa4.value}</div>`;
-    newDiv.innerHTML += `<div>Описание${spa5.value}</div>`;
-    elements.append(newDiv);
+    newDiv.innerHTML += `<div>Цена:${spa2.value}</div>`;
+    newDiv.innerHTML += `<div>Количество:${spa3.value}</div>`;
+    newDiv.innerHTML += `<div>Артикул:${spa4.value}</div>`;
+    newDiv.innerHTML += `<div>Описание:${spa5.value}</div>`;
 
     let deleteDiv = document.createElement('button')
+    deleteDiv.innerText = 'удалить'
+    deleteDiv.style.backgroundColor = '#830101'
+    deleteDiv.style.color = '#fddbdb'
+    deleteDiv.style.borderRadius = '5px'
     newDiv.append(deleteDiv)
-    deleteButton.innerHTML = 'Удалить'
     function deleteBut() {
-        newDiv.remove()
-    }
+        if (confirm("Вы точно хотите удалить товар?")) {
+            newDiv.remove()
+        } else {
+            alert('Вы отменили удаление товара')
+        }
 
+    }
     deleteDiv.addEventListener('click', deleteBut)
-    elements.append(newDiv, deleteDiv)
+    elements.append(newDiv)
 }
 
 button.addEventListener('click', appendElement);
@@ -55,6 +60,17 @@ function buttonInput5() {
     spa5.value = ''
 }
 but2.addEventListener('click', buttonInput5)
+
+function deleteVse(){
+
+        if (confirm("Вы точно хотите удалить все товары?")) {
+            elements.innerHTML = ""
+        } else {
+            alert('Вы отменили удаление всех товаров')
+        }
+}
+uvt.addEventListener('click',deleteVse)
+
 
 
 
